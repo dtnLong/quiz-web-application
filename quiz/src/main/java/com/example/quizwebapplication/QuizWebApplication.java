d90aeefea5e174e2460b0031e034b2e8077eea6e
dtnLong <49140143+dtnLong@users.noreply.github.com>
2021-06-20 23:21:46 +0700

    Update backend template

--- quiz/src/main/java/com/example/quizwebapplication/QuizWebApplication.java
+++ quiz/src/main/java/com/example/quizwebapplication/QuizWebApplication.java
@@ -1,13 +1,13 @@
-package com.example.quiz;
+package com.example.quizwebapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizApplication {
+public class QuizWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuizApplication.class, args);
+        SpringApplication.run(QuizWebApplication.class, args);
     }
 
 }
