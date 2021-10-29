0991119996417ed1320ba8359c04e16be7a16e21
minhquan1701 <64230078+minhquan1701@users.noreply.github.com>
2021-10-29 18:05:20 +0700

    update static folder

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import './main.css'
 // import router from './router'
 // createApp(App).use(router).mount('#app')
+Vue.config.devtools = false;
 createApp(App).mount('#app');
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -48,7 +48,7 @@ export default {
     })
 
 
-    const isLogin = ref(true);
+    const isLogin = ref(false);
     const isLoading = ref(false);
     const groupName = ref(null);
     const quizCode = ref(null);
